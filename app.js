6df8693cc4bb5efabad6c3aea27f7315616b9478
gouthm.04 <gouthm.04@gmail.com>
2026-07-15 23:23:21 +0530

    ..

--- app.js
+++ app.js
@@ -1,16 +1,80 @@
-const Express  = require("express")
-const Cors  = require("cors")
-const Mongoose  = require("mongoose")
-const jwt  = require("jsonwebtoken")
-const Bcypt  = require("bcrypt")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const userModel = require("./models/Users");
 
-let  app = Express()
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+mongoose.connect(
+  "mongodb+srv://goutham:goutham123@cluster0.umdwywd.mongodb.net/blogDB",
+);
 
-app.get("/", (req,res) => {
-    res.send("hellooo")
+//SIGN IN
+app.post("/signIn",async(req,res)=>{
+
+    let input =req.body
+    let result=userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if (items.length>0) {
+
+                const passwordValidator=bcrypt.compareSync(req.body.password,items[0].password)
+                if (passwordValidator) {
+
+                    jwt.sign({email:req.body.email},"blogApp",{expiresIn:"1d"},
+                        (error,token)=>{
+                            if (error) {
+                                res.json({"status":"error","errorMessage":error})
+                                
+                            } else {
+                                res.json({"status":"success","token":token,"userId":items[0]._id})
+                                
+                            }
+                        })
+                    
+                } else {
+                    res.json({"status":"Incorrect Password"})
+                    
+                }
+                
+            } else {
+                res.json({"status":"Invalid Email Id"})
+                
+            }
+
+        }
+    )
 })
 
-app.listen(3000,() => {
-    console.log("Server Started")
-})
+//SIGN UP
+app.post("/signup", (req, res) => {
+  let input = req.body;
+  let hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+
+  userModel
+    .find({ email: req.body.email })
+    .then((items) => {
+      if (items.length > 0) {
+        res.json({ status: "Email ID already exists " });
+      } else {
+        let result = new userModel(input);
+        result.save();
+        res.json({ status: "success" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/test", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
